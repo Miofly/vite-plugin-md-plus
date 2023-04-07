c09d9462b900ef89bf5ca8f0abac6ab2c833e49b
wfly <953212389@qq.com>
2023-04-07 15:57:17 +0800

    feat: 1

--- src/main.ts
+++ src/main.ts
@@ -9,31 +9,31 @@ import path, { join, basename } from 'path';
 
 async function createMarkdown (options: any) {
   const { markdown = {} } = options;
-  
+
   const md = await createMarkdownRenderer(markdown);
   if (options.markdown && options.markdown.init) options.markdown.init({ md });
-  
+
   return (source: string, file: string) => {
-    
+
     const { transforms = {} } = options;
     let result = source;
     if (transforms.before) source = transforms.before({ source, file, md });
-    
+
     if (transforms.render) {
       result = transforms.render({ source, file, md });
     } else {
       result = md.render(source);
     }
-    
+
     if (transforms.after) result = transforms.after({ result, source, file, md });
-    
+
     return result;
   };
 }
 
 async function markdownPlugin (userOptions = {}) {
   const mdRender = await createMarkdown(userOptions);
-  
+
   return {
     name: 'vite-plugin-md-plus',
     enforce: 'pre',
@@ -43,22 +43,22 @@ async function markdownPlugin (userOptions = {}) {
     },
     resolveId (id: any) {
       if (!id.endsWith('.md')) return null;
-      
+
       const filePath = join(process.cwd(), id);
       const fileContent = fs.readFileSync(filePath, 'utf8');
       const frontMatter = matter(fileContent, {});
-  
+
       if (Object.keys(frontMatter.data).length === 0) {
         // const dateStr = dateFormat(getBirthtime(stat)); // 文件的创建时间
         const stat = fs.statSync(filePath);
-  
+
         const category = getcategory(filePath);
         let cateLabelStr = '';
         category.forEach((item) => {
           cateLabelStr += os.EOL + '  - ' + item;
         });
-  
-        
+
+        let cateStr;
         let title;
         let tagsStr;
         let path;
@@ -68,14 +68,14 @@ async function markdownPlugin (userOptions = {}) {
           tagsStr = `
 tags:
   - `;
-          path = buildShortUUID(10)
+          path = buildShortUUID(10);
+          cateStr = cateLabelStr ? os.EOL + 'category:' + cateLabelStr : '';
         } else {
           title = fileTitleArr[0];
           tagsStr = '';
-          path = fileTitleArr[0]
+          path = fileTitleArr[0];
+          cateStr = cateLabelStr ? os.EOL + 'category:' + os.EOL + '  - ' + 'api' + cateLabelStr : '';
         }
-        
-        const cateStr = cateLabelStr ? os.EOL + 'category:' + cateLabelStr : '';
 
         const fmData = `---
 title: ${title}
@@ -90,10 +90,10 @@ path: /pages/${path}${cateStr}${tagsStr}
 
 function getcategory (filePath: string) {
   const category = [];
-  
+
   const filePathArr = filePath.split(path.sep); // path.sep用于兼容不同系统下的路径斜杠
   filePathArr.pop();
-  
+
   let ind = filePathArr.indexOf('docs');
   if (ind !== -1) {
     while (filePathArr[++ind] !== undefined) {
