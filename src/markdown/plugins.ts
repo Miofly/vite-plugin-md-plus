06282e262d03b51c817e4357d18476465ccf56e3
wfly <953212389@qq.com>
2024-03-11 20:13:24 +0800

    fix: update

--- src/markdown/plugins.ts
+++ src/markdown/plugins.ts
@@ -14,10 +14,10 @@ export { sfcPlugin, type SfcPluginOptions } from '@mdit-vue/plugin-sfc';
 export { titlePlugin } from '@mdit-vue/plugin-title';
 export { tocPlugin, type TocPluginOptions } from '@mdit-vue/plugin-toc';
 
-// export { anchorPlugin, type AnchorPluginOptions } from './plugins/anchorPlugin';
-// export { assetsPlugin, type AssetsPluginOptions } from './plugins/assetsPlugin';
-// export { codePlugin, type CodePluginOptions } from './plugins/codePlugin';
-// export { emojiPlugin, type EmojiPluginOptions } from './plugins/emojiPlugin';
+export { anchorPlugin, type AnchorPluginOptions } from './plugins/anchorPlugin';
+export { assetsPlugin, type AssetsPluginOptions } from './plugins/assetsPlugin';
+export { codePlugin, type CodePluginOptions } from './plugins/codePlugin';
+export { emojiPlugin, type EmojiPluginOptions } from './plugins/emojiPlugin';
 // export {
 //   importCodePlugin,
 //   type ImportCodePluginOptions,
